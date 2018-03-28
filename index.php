27789844141079f2dde024fabf30d98f03d4e5d3
jesus david <jesusmosquera_@hotmail.com>
2018-03-28 13:57:24 -0500

    prueba

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<?php
+$dbname=getenv("POSTGRESQL_DATABASE");
+//$dbuser=getenv("POSTGRESQL_USER");
+$dbpassword=getenv("POSTGRESQL_PASSWORD");
+//$dbport=getenv("5432");
+$conn=pg_connect("dbname='$dbname' password='$dbpassword'");   
+
+if (!$conn) {
+  echo "Ocurri� un error.\n";
+  exit;
+}
+
+$result = pg_query($conn, "SELECT * FROM usuario");
+if (!$result) {
+  echo "Ocurrio un error.\n";
+  exit;
+}
+
+while ($row = pg_fetch_row($result)) {
+  echo "id: $row[0] nombre: $row[1] apellido: $row[2]";
+  echo "<br/>";
+}
+?>
